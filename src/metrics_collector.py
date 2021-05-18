a48f1bf37c5d70af9898d1f03cdf48163e51d76b
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-05-18 02:42:00 +0200

    adjust formatting and polish metrics collector
    - add log to metrics collector
    - fix data formatting in case of connection error
    - adjust integration tests to changes

--- src/metrics_collector.py
+++ src/metrics_collector.py
@@ -27,7 +27,7 @@ def get_metrics(url: str, re_pattern: Optional[str] = None) -> Dict:
         resp = requests.get(url, headers=headers, stream=True)
     except requests.exceptions.ConnectionError:
         result = {
-            'request_timestamp': None,
+            'request_timestamp': request_time.strftime('%Y-%m-%d %H:%M:%S'),
             'url': None,
             'ip_address': None,
             'resp_time': None,
@@ -35,6 +35,7 @@ def get_metrics(url: str, re_pattern: Optional[str] = None) -> Dict:
             'pattern_found': False if re_pattern else None,
             'service_name': SERVICE_NAME
         }
+        log.warning(f'Not possible to reach url {url}!')
         return result
     rest = parse_response(resp, re_pattern)
     log.info(f'Received response from: {url}. Within: {rest[1]} with status: {rest[2]}')
--- tests/integration/test_web_metrics_collector.py
+++ tests/integration/test_web_metrics_collector.py
@@ -16,8 +16,12 @@ RE_PATTERN_INVALID = 'I find your lack of faith disturbing'
 @pytest.mark.slow
 def test_behavior_with_invalid_http_response():
     result = get_metrics('https://www.monedo.jpy/')
+    try:
+        datetime.strptime(result['request_timestamp'], '%Y-%m-%d %H:%M:%S')
+    except ValueError as e:
+        pytest.fail(f"{result['request_timestamp']} has wrong format {e.args}")
+    result.pop('request_timestamp')
     assert result == {
-        'request_timestamp': None,
         'url': None,
         'ip_address': None,
         'resp_time': None,
@@ -33,13 +37,22 @@ def test_behavior_with_valid_http_response_and_no_pattern():
     # Due to datetime.utcnow() this test may fail during manual debug!
     result = get_metrics(ALWAYS_AVAILABLE_URL)
     max_timespan = timedelta(milliseconds=MAXIMUM_ACCEPTABLE_RESP_TIME)
+    try:
+        time = datetime.strptime(result['request_timestamp'], '%Y-%m-%d %H:%M:%S')
+    except ValueError as e:
+        pytest.fail(f"{result['request_timestamp']} has wrong format {e.args}")
     msg = f'Response to {ALWAYS_AVAILABLE_URL} either unsuccessful or took more than {str(max_timespan)}'
-    assert max_timespan > datetime.utcnow() - result['request_timestamp'] > timedelta(milliseconds=0), msg
+    assert max_timespan > datetime.utcnow() - time > timedelta(milliseconds=0), msg
     assert result['url'] == ALWAYS_AVAILABLE_URL
     msg = f'IP address seems have invalid format. Got: {result["url"]}'
     assert ipaddress.ip_address(result['ip_address']), msg
+    try:
+        t = datetime.strptime(result['resp_time'], '%H:%M:%S.%f')
+        time = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second, microseconds=t.microsecond)
+    except ValueError as e:
+        pytest.fail(f"{result['request_timestamp']} has wrong format: {e.args}")
     msg = f'Elapsed argument for {ALWAYS_AVAILABLE_URL} took longer than {str(max_timespan)}'
-    assert result['resp_time'] < max_timespan, msg
+    assert time < max_timespan, msg
     assert result['resp_status_code'] == 200
     assert result['pattern_found'] is None
     assert result['service_name'] is SERVICE_NAME
