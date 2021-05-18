0dfd2d9aac8232d76fedec3daf50016c199e2c28
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-05-19 00:37:23 +0200

    add smoke tests for producer readiness

---
+++ tests/integration/test_smoke.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.service import TOPIC, aiven_kafka_producer as producer
+
+
+"""This test ensure that kafka producer is properly configured
+and the overall setup (including Aiven kafka broker) is working"""
+
+
+@pytest.mark.smoke
+@pytest.mark.slow
+def test_smoke_kafka_producer():
+    with producer:
+        producer._producer.config['max_block_ms'] = 3000
+        producer.send(TOPIC, {'comment': 'test'})
