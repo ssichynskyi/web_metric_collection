03e00e2f6a402f62e84573c856bd7a045e85e63b
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-05-19 18:39:31 +0200

    fix defect with garbage messaging
    message was sent in a corrupt format and because of other known issue
    resulted in entire readout was not recorded to DB

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.service import TOPIC, aiven_kafka_producer as producer
+from src.service import TOPIC, AIVEN_KAFKA_PRODUCER as producer
 
 
 """This test ensure that kafka producer is properly configured
@@ -11,4 +11,16 @@ and the overall setup (including Aiven kafka broker) is working"""
 def test_smoke_kafka_producer():
     with producer:
         producer._producer.config['max_block_ms'] = 3000
-        producer.send(TOPIC, {'comment': 'test'})
+        producer.send(TOPIC, data)
+
+
+data = {
+    'request_timestamp': '2021-01-01 00:00:00',
+    'url': 'https://www.monedo.com/',
+    'ip_address': '104.18.91.87',
+    'resp_time': '0:00:00.123456',
+    'resp_status_code': 200,
+    'pattern_found': True,
+    'service_name': 'Web metric collection service',
+    'comment': 'test'
+}
