6be6beb72c596182085e9fb7a04e787cab8118fc
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-06-04 00:11:27 +0200

    add files to new approach with configs

--- utils/env_config.py
+++ utils/env_config.py
@@ -4,9 +4,11 @@ from pathlib import Path
 
 import yaml
 
+from utils.dict_merger import merge_dicts
+
 
 class ConfigParser:
-    """Extracts the data from the configuration file given"""
+    """Extracts the data from the configuration file given."""
     def __new__(cls, path):
         with open(path, 'r') as f:
             contents = f.read()
@@ -14,4 +16,7 @@ class ConfigParser:
 
 
 _path_to_config = Path(os.environ['COLLECTION_CONFIG'])
+_path_to_local_config = Path(os.environ['COLLECTION_CONFIG_LOCAL'])
 config = ConfigParser(_path_to_config)
+_config_local = ConfigParser(_path_to_local_config)
+config = merge_dicts(config, _config_local)
